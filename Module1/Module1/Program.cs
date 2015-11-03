4961f9195a90c54b3611c7946c4b508acb45ee4f
Alban316 <jason.brugger@gmail.com>
2015-11-03 16:26:50 -0500

    asdfasdf

---
+++ Module1/Module1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+            // birthDate;
+            string addressLine1;
+            string addressLine2;
+            string city;
+            string stateProvince;
+            string zipPostal;
+            string country;
+
+        }
+    }
+}
