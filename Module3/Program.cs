cdba280f26b9860cc25710fde57799530358e5fc
Alban316 <jason.brugger@gmail.com>
2016-01-25 10:19:12 -0500

    wtf

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -8,30 +8,110 @@ namespace Module3
 {
     class Program
     {
-        //GetStudentInformation()
 
-        //PrintStudentDetails()
+        class Course
+        {
+            private string teacherName;
+            private string courseTitle;
+            private string programTitle;
 
-        /*
-        Using the above partial code sample, complete the method for getting student data.
+            public void GetCourseInformation()
+            {
+                Console.Write("Enter course title: ");
+                courseTitle = Console.ReadLine();
 
-        Create a method to get information for a teacher, a course, a uprogram, and a degree using a similar method as above.
+                Console.Write("Enter course instructor name: ");
+                teacherName = Console.ReadLine();
 
-        Create methods to print the information to the screen for each object such as static void PrintStudentDetails(...).
-            
-        From within Main(), call each of the methods to prompt for input from a user of your application.
-            
-        Just enter enough information to show you understand how to use methods.  (At least three attributes each).
-            
-        Assign the values that are input, to the proper variables.
-            
-        Output the values of each object using the "print" methods that you created.
-        */
+                Console.Write("Enter program name, e.g. 'Computer Science': ");
+                programTitle = Console.ReadLine();
+            }
+
+            public string TeacherName
+            {
+                get
+                {
+                    return teacherName;
+                }
+
+            }
+            public string CourseTitle
+            {
+                get
+                {
+                    return courseTitle;
+                }
+            }
+
+            public string ProgramTitle
+            {
+                get
+                {
+                    return programTitle;
+                }
+            }
+
+        }
+
+        class Student
+        {
+            private string firstName;
+            private string lastName;
+            private DateTime birthDate;
+            private Course course;
+
+            public void ValidateStudentBirthdate()
+            {
+                throw new NotImplementedException();
+            }
+
+
+            public void GetStudentInformation()
+            {
+                Console.Write("Enter the student's First name: ");
+                firstName = Console.ReadLine();
+
+                Console.Write("Enter the student's Last name: ");
+                lastName = Console.ReadLine();
+
+                Console.Write("Enter the student's Date of Birth: ");
+                birthDate = Convert.ToDateTime(Console.ReadLine());
+
+                course = new Course();
+                course.GetCourseInformation();
+            }
+
+            public void PrintStudentDetails()
+            {
+                Console.WriteLine("{0} {1}, born {2}, is taking course {3}, instructed by {4} in the {5} program.", firstName, lastName, birthDate.ToShortDateString(), course.CourseTitle, course.TeacherName, course.ProgramTitle);
+            }
+
+        }
+
 
         static void Main(string[] args)
         {
 
-            //NotImplementedException exception.
+            try
+            {
+                Student student = new Student();
+                student.GetStudentInformation();
+
+                student.PrintStudentDetails();
+
+                student.ValidateStudentBirthdate(); // should throw exception since it is not implemented
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine("Caught exception {0}", e);
+            }
+
+            finally
+            {
+                Console.WriteLine("Press Enter to exit...");
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ PeerCheck/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_3_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call GET methods.
+            GetStudentInfo();
+            GetTeacherInfo();
+            GetDegreeInfo();
+            GetProgramInfo();
+            GetCourseInfo();
+        }
+
+        static void GetStudentInfo()
+        {
+            // Get data.
+            Console.WriteLine("Enter the student's first name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter the student's last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter the student's birthday (dd/mm/aaaa): ");
+            DateTime birthday = ValidateBirthday(Console.ReadLine());
+
+            // Print data.
+            PrintStudentDetails(firstName, lastName, birthday);
+        }
+
+        static void PrintStudentDetails(string first, string last, DateTime birthday)
+        {
+            Console.WriteLine("The student {0} {1} was born on: {2}.\n", first, last, birthday);
+        }
+
+        static void GetTeacherInfo()
+        {
+            // Get data.
+            Console.WriteLine("Enter the teacher's first name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter the teacher's last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter the teacher's birthday: ");
+            DateTime birthday = ValidateBirthday(Console.ReadLine());
+
+            // Print data.
+            PrintTeacherDetails(firstName, lastName, birthday);
+        }
+
+        static void PrintTeacherDetails(string first, string last, DateTime birthday)
+        {
+            Console.WriteLine("The teacher {0} {1} was born on: {2}.\n", first, last, birthday);
+        }
+
+        static void GetDegreeInfo()
+        {
+            // Get data.
+            Console.WriteLine("Enter the name of the degree: ");
+            string degreeName = Console.ReadLine();
+            Console.WriteLine("Enter the school: ");
+            string school = Console.ReadLine();
+            Console.WriteLine("Enter the total number of credits: ");
+            int totalCredits = Convert.ToInt32(Console.ReadLine());
+
+            // Print data.
+            PrintDegreeDetails(degreeName, school, totalCredits);
+        }
+
+        static void PrintDegreeDetails(string name, string school, int credits)
+        {
+            Console.WriteLine("The {0} is imparted at {1}. Credits: {2}.\n", name, school, credits);
+        }
+
+        static void GetProgramInfo()
+        {
+            // Get data.
+            Console.WriteLine("Enter the name of the program: ");
+            string programName = Console.ReadLine();
+            Console.WriteLine("Enter the degrees offered in the program: ");
+            string degreesOffered = Console.ReadLine();
+            Console.WriteLine("Enter the name of the department head: ");
+            string departmentHead = Console.ReadLine();
+
+            // Print data.
+            PrintProgramDetails(programName, degreesOffered, departmentHead);
+        }
+
+        static void PrintProgramDetails(string name, string dOffered, string departmentH)
+        {
+            Console.WriteLine("The {0} offer {1} degrees. Department head: {2}.\n", name, dOffered, departmentH);
+        }
+
+        static void GetCourseInfo()
+        {
+            // Get data.
+            Console.WriteLine("Enter the name of the course: ");
+            string courseName = Console.ReadLine();
+            Console.WriteLine("Enter the field of study: ");
+            string studyField = Console.ReadLine();
+            Console.WriteLine("Enter the course credits: ");
+            int courseCredits = Convert.ToInt32(Console.ReadLine());
+
+            // Print data.
+            PrintCourseDetails(courseName, studyField, courseCredits);
+        }
+
+        static void PrintCourseDetails(string name, string field, int credits)
+        {
+            Console.WriteLine("Course: {0}. Field of study: {1}. Credits: {2}.\n", name, field, credits);
+        }
+
+        static DateTime ValidateBirthday(String date)
+        {
+            try
+            {
+                DateTime birthday = DateTime.Parse(date);
+
+                // Check if it is greater than 18 years old.
+                if (birthday.AddYears(18).CompareTo(DateTime.Today) > 0)
+                {
+                    Console.WriteLine("The student's age is less than 18.");
+                }
+
+                return birthday;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid date format.");
+                return DateTime.Today;
+            }
+        }
+    }
+}
