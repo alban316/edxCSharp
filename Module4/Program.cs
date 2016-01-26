b55e754c9120df1a83f9ad207b7958a3f4b393ab
Alban316 <jason.brugger@gmail.com>
2016-01-26 15:57:05 -0500

    mod 4 updates

---
+++ Module4/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module4
+{
+    class Module4
+    {
+        public struct Student
+        {
+            public Student(string firstName, string lastName, string address, string city)
+            {
+                this.FirstName = firstName;
+                this.LastName = lastName;
+                this.Address = address;
+                this.City = city;
+            }
+
+            public string FirstName;
+            public string LastName;
+            public string Address;
+            public string City;
+        }
+
+        public struct Teacher
+        {
+            public Teacher(string firstName, string lastName, string department)
+            {
+                this.FirstName = firstName;
+                this.LastName = lastName;
+                this.Department = department;
+            }
+
+            public string FirstName;
+            public string LastName;
+            public string Department;
+        }
+
+        public struct Program
+        {
+            public Program(string programName)
+            {
+                this.ProgramName = programName;
+            }
+
+            public string ProgramName;
+        }
+
+        public struct Course
+        {
+            public Course(string courseTitle, int numCredits)
+            {
+                this.CourseTitle = courseTitle;
+                this.NumCredits = numCredits;
+            }
+
+            public string CourseTitle;
+            public int NumCredits;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Student[] student = new Student[5];
+            student[0] = new Student("Pebbles", "Flintstone", "55 Cobblestone Rd", "Bedrock");
+
+            Console.WriteLine("{0} {1}", student[0].FirstName, student[0].LastName);
+            Console.WriteLine("{0}", student[0].Address);
+            Console.WriteLine("{0}", student[0].City);
+        }
+    }
+}
--- PeerCheck/Program.cs
+++ PeerCheck/Program.cs
@@ -4,134 +4,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Module_3_Assignment
+namespace edxCSharp
 {
-    class Program
+    public struct Student
+    {
+        public string firstName;
+        public string lastName;
+        public string birthDate;
+        public string[,] myClass;
+        public int count;
+
+        public Student(string fName, string lName, string bDate)
+        {
+            count = 0;
+            this.firstName = fName;
+            this.lastName = lName;
+            this.birthDate = bDate;
+            myClass = new string[,] { { this.firstName, this.lastName, this.birthDate } };
+        }
+
+
+    }
+
+    public struct Teacher
+    {
+        public string firstName;
+        public string lastName;
+        public string birthDate;
+
+        public Teacher(string fName, string lName, string bDate)
+        {
+            this.firstName = fName;
+            this.lastName = lName;
+            this.birthDate = bDate;
+        }
+    }
+
+    public struct Program
+    {
+        public string programName;
+        public string deptHead;
+        public int degrees;
+
+        public Program(string pName, string dHead, int Dgrees)
+        {
+            this.programName = pName;
+            this.deptHead = dHead;
+            this.degrees = Dgrees;
+        }
+    }
+
+    public struct Course
+    {
+        public string courseName;
+        public string credits;
+        public string durationInWeeks;
+        public string nameOfTeacher;
+
+        public Course(string cName, string kredit, string dInWeeks, string nOfTeacher)
+        {
+            this.courseName = cName;
+            this.credits = kredit;
+            this.durationInWeeks = dInWeeks;
+            this.nameOfTeacher = nOfTeacher;
+        }
+    }
+
+    public class Mod4Assign
     {
         static void Main(string[] args)
         {
-            // Call GET methods.
-            GetStudentInfo();
-            GetTeacherInfo();
-            GetDegreeInfo();
-            GetProgramInfo();
-            GetCourseInfo();
-        }
-
-        static void GetStudentInfo()
-        {
-            // Get data.
-            Console.WriteLine("Enter the student's first name: ");
-            string firstName = Console.ReadLine();
-            Console.WriteLine("Enter the student's last name: ");
-            string lastName = Console.ReadLine();
-            Console.WriteLine("Enter the student's birthday (dd/mm/aaaa): ");
-            DateTime birthday = ValidateBirthday(Console.ReadLine());
-
-            // Print data.
-            PrintStudentDetails(firstName, lastName, birthday);
-        }
-
-        static void PrintStudentDetails(string first, string last, DateTime birthday)
-        {
-            Console.WriteLine("The student {0} {1} was born on: {2}.\n", first, last, birthday);
-        }
-
-        static void GetTeacherInfo()
-        {
-            // Get data.
-            Console.WriteLine("Enter the teacher's first name: ");
-            string firstName = Console.ReadLine();
-            Console.WriteLine("Enter the teacher's last name: ");
-            string lastName = Console.ReadLine();
-            Console.WriteLine("Enter the teacher's birthday: ");
-            DateTime birthday = ValidateBirthday(Console.ReadLine());
-
-            // Print data.
-            PrintTeacherDetails(firstName, lastName, birthday);
-        }
-
-        static void PrintTeacherDetails(string first, string last, DateTime birthday)
-        {
-            Console.WriteLine("The teacher {0} {1} was born on: {2}.\n", first, last, birthday);
-        }
-
-        static void GetDegreeInfo()
-        {
-            // Get data.
-            Console.WriteLine("Enter the name of the degree: ");
-            string degreeName = Console.ReadLine();
-            Console.WriteLine("Enter the school: ");
-            string school = Console.ReadLine();
-            Console.WriteLine("Enter the total number of credits: ");
-            int totalCredits = Convert.ToInt32(Console.ReadLine());
-
-            // Print data.
-            PrintDegreeDetails(degreeName, school, totalCredits);
-        }
-
-        static void PrintDegreeDetails(string name, string school, int credits)
-        {
-            Console.WriteLine("The {0} is imparted at {1}. Credits: {2}.\n", name, school, credits);
-        }
-
-        static void GetProgramInfo()
-        {
-            // Get data.
-            Console.WriteLine("Enter the name of the program: ");
-            string programName = Console.ReadLine();
-            Console.WriteLine("Enter the degrees offered in the program: ");
-            string degreesOffered = Console.ReadLine();
-            Console.WriteLine("Enter the name of the department head: ");
-            string departmentHead = Console.ReadLine();
-
-            // Print data.
-            PrintProgramDetails(programName, degreesOffered, departmentHead);
-        }
-
-        static void PrintProgramDetails(string name, string dOffered, string departmentH)
-        {
-            Console.WriteLine("The {0} offer {1} degrees. Department head: {2}.\n", name, dOffered, departmentH);
-        }
-
-        static void GetCourseInfo()
-        {
-            // Get data.
-            Console.WriteLine("Enter the name of the course: ");
-            string courseName = Console.ReadLine();
-            Console.WriteLine("Enter the field of study: ");
-            string studyField = Console.ReadLine();
-            Console.WriteLine("Enter the course credits: ");
-            int courseCredits = Convert.ToInt32(Console.ReadLine());
-
-            // Print data.
-            PrintCourseDetails(courseName, studyField, courseCredits);
-        }
-
-        static void PrintCourseDetails(string name, string field, int credits)
-        {
-            Console.WriteLine("Course: {0}. Field of study: {1}. Credits: {2}.\n", name, field, credits);
-        }
-
-        static DateTime ValidateBirthday(String date)
-        {
-            try
-            {
-                DateTime birthday = DateTime.Parse(date);
-
-                // Check if it is greater than 18 years old.
-                if (birthday.AddYears(18).CompareTo(DateTime.Today) > 0)
-                {
-                    Console.WriteLine("The student's age is less than 18.");
-                }
-
-                return birthday;
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Invalid date format.");
-                return DateTime.Today;
-            }
+            Student student1 = new Student("John", "Michel", "10/03/1985");
+            Student student2 = new Student("Obi", "John", "10/03/1978");
+            Student student3 = new Student("Michel", "Obi", "10/25/1989");
+            Console.WriteLine("\t\tSTUDENT DETAILS");
+            Console.WriteLine("S/N\tFirstName\tLastName\tBirthdate");
+            Console.WriteLine("{0}\t{1}\t\t{2}\t\t{3}", i + 1, student1.firstName, student1.lastName, student1.birthdate);
+            Console.WriteLine("{0}\t{1}\t\t{2}\t\t{3}", i + 2, student2.firstName, student2.lastName, student2.birthdate);
+            Console.Write("\nPress Enter key to continue...");
+            Console.ReadLine();
         }
     }
 }
